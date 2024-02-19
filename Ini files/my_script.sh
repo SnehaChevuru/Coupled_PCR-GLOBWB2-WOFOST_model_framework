20a78608d1707a78006ac6cd342d7539854755e5
chevurusneha <snehareddychevuru@gmail.com>
2024-02-19 17:56:25 +0100

    Initial commit
    Initial commit

---
+++ Ini files/my_script.sh
@@ -0,0 +1,21 @@
+#Maize compiling
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Maggy/WOFOSTBMI_Maggy/_build/
+make install
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Maggy/pymt_Maggy/
+make install
+
+#Soybean compiling
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Saggy/WOFOSTBMI_Saggy/_build/
+make install
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Saggy/pymt_Saggy/
+make install
+
+#Wheat compiling
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Weggy/WOFOSTBMI_Weggy/_build/
+make install
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Weggy/pymt_Weggy/
+make install
+
+
+cd /eejit/home/chevu001/PCR-WOFOST/TW/Pcrglobwb/
+
